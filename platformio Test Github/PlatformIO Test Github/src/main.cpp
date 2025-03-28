a9db638744c1e25a382dd36f5e23cf634d1a5906
LilNutbush <gumbafreak@gmail.com>
2025-03-28 19:16:18 +0100

    final?

--- platformio Test Github/PlatformIO Test Github/src/main.cpp
+++ platformio Test Github/PlatformIO Test Github/src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-const int rs = 3, e = 4, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
-LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
-const int pump[] = {10, 11, 12};
+const int pump[] = {5, 6, 7};
 const int sens[] = {0, 1, 2};
 int val[sizeof(sens)];
 
-const int pumptime = 1000;
+const int pumptime = 160000;
 const int pumpval = 500;
 
 void setup()
 {
-  lcd.begin(16, 2);
   for (int i = 0; i < 3; i++)
   {
     pinMode(pump[i], OUTPUT);
@@ -25,13 +22,6 @@ void loop()
   {
     val[i] = analogRead(sens[i]);
   }
-
-  lcd.setCursor(0, 0);
-  lcd.print(val[0]);
-  lcd.setCursor(5, 0);
-  lcd.print(val[1]);
-  lcd.setCursor(10, 0);
-  lcd.print(val[2]);
 
   for (int i = 0; i < 3; i++)
   {
