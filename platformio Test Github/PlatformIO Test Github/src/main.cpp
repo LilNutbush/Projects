b38747758e51974c19177789b8f330c4bda1a04c
LilNutbush <gumbafreak@gmail.com>
2025-04-05 15:08:20 +0200

    nop

--- platformio Test Github/PlatformIO Test Github/src/main.cpp
+++ platformio Test Github/PlatformIO Test Github/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-const int pump[] = {6, 7, 8};
-const int sens[] = {0, 2, 5};
+const int pump[] = {7, 8};
+const int sens[] = {2, 5};
 int val[sizeof(sens)];
+int nop = sizeof(sens); // number of pots
 
 const long pumptime = 160000;
 const int pumpval = 500;
 
 void setup()
 {
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < nop; i++)
   {
     pinMode(pump[i], OUTPUT);
     digitalWrite(pump[i], HIGH);
@@ -18,12 +19,12 @@ void setup()
 
 void loop()
 {
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < nop; i++)
   {
     val[i] = analogRead(sens[i]);
   }
 
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < nop; i++)
   {
     if (val[i] >= pumpval)
     {
