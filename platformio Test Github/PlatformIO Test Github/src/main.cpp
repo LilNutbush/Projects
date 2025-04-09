0c96e498fcb7f5c32db7f3ff4fc8da640d662407
LilNutbush <gumbafreak@gmail.com>
2025-04-09 19:44:55 +0200

    pumpdivider and pumpwait set

--- platformio Test Github/PlatformIO Test Github/src/main.cpp
+++ platformio Test Github/PlatformIO Test Github/src/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
 
-const int pump[] = {7, 8};    // pumpen stecker
-const int sens[] = {2, 5};    // sensor stecker
+const int pump[] = {6, 7, 8}; // pumpen stecker
+const int sens[] = {0, 2, 5}; // sensor stecker
 const int nop = sizeof(sens); // anzahl an pflanzen(nop = number of pots)
 int val[nop];                 // variable für sensorwerte
 
 const long pumptime = 100000; // gesamte gießzeit
-const int pumpdivider = 2;    // Aufteilung der gießzeit
+const int pumpdivider = 15;   // Aufteilung der gießzeit
+const long pumpwait = 60000;
 
 const int pumpval = 500; // Schwellwert für sensor
 
@@ -35,7 +36,7 @@ void loop()
         digitalWrite(pump[i], LOW);
         delay(pumptime / pumpdivider);
         digitalWrite(pump[i], HIGH);
-        delay(pumptime);
+        delay(pumpwait);
       }
     }
   }
